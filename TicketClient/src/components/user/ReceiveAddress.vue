8aa020596c06c41f412f128e629e1c1e7654eee9
KingOfSolo <944125893@qq.com>
2018-02-17 21:16:46 +0800

    收货地址管理

---
+++ TicketClient/src/components/user/ReceiveAddress.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="receive-address">
+
+  </div>
+</template>
+
+<script>
+  export default{
+    data () {
+      return{
+
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
